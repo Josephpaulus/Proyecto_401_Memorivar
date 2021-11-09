ef0e585ffe36d720239046d10b152501a9c18d12
Weroleytor <ulises_heraldez@hotmail.com>
2021-11-09 04:36:52 -0700

    explore_courses

--- src/app/courses/explore-courses/explore-courses.component.ts
+++ src/app/courses/explore-courses/explore-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListCourses } from '../courses';
 
 @Component({
   selector: 'app-explore-courses',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore-courses.component.scss']
 })
 export class ExploreCoursesComponent implements OnInit {
+  listCourses: ListCourses[] = [];
+  message: string = "";
+
+  recibirMensaje($event: any): void {
+    this.message = $event;
+    console.log(this.message);
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 10; i++) {
+      const course: ListCourses = {
+        name: `Curso #${i + 1}`,
+        image: 'https://material.angularjs.org/1.1.1/img/list/60.jpeg',
+        wordsLearned: this.rand(0, 10),
+        totalWords: this.rand(0, 100),
+        progress: this.rand(0, 40),
+        reviewIn: this.rand(1, 3),
+      };
+      this.listCourses.push(course);
+    }
   }
 
+  rand(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 }
