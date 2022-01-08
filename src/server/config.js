c125b815e68cc66eb5b0036ede03ada8d61ab793
Weroleytor <ulises_heraldez@hotmail.com>
2022-01-07 18:10:28 -0700

    datos_usuario_actualizados

--- src/server/config.js
+++ src/server/config.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 // Set database connection credentials
 const config = {
-    host: '185.156.219.141',
-    user: 'i161927_admin',
+    host: '185.156.219.153',
+    user: 'b177337_admin',
     password: '1IB_3LVC(H+6',
     port: 3306,
-    database: 'i161927_memorivar_db'
+    database: 'b177337_memorivar_db'
 };
 
 // Create a MySQL pool
